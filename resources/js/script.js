99bb58e53485f3e2b5587b7a798ea46c17ffa744
chaitanya151 <70018620+chaitanya151@users.noreply.github.com>
2020-09-26 00:04:52 +0530

    First Website
    I hope it works.

---
+++ resources/js/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+   
+   $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down"){
+            $('nav').addclass('sticky');
+        } else{
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+  })
+    });
+    /*var waypoints = $('#handler-first').waypoint(function(direction) {
+    notify(this.element.id + ' hit 25% from top of window') 
+    }, {
+    offset: '25%'
+    })*/
+
+    
+    /*Scroll on Buttons*/
+    $('.js--scroll-to-plans').click(function () {
+        $('html,body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+        $('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000); 
+    });
+    
+    
+    /*Navigation*/
+    $(function () {
+        $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+     });
+});
+    
+/*Animation on scroll*/
